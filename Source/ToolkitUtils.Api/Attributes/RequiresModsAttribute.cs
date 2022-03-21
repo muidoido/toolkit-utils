28d1b154e6848d023e625443a4aa25720c1e3e19
SirRandoo <8146689+SirRandoo@users.noreply.github.com>
2022-03-21 18:35:20 -0500

    Outlined ways to define commands.

---
+++ Source/ToolkitUtils.Api/Attributes/RequiresModsAttribute.cs
@@ -0,0 +1,35 @@
+﻿// ToolkitUtils.Api
+// Copyright (C) 2022  SirRandoo
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+// 
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+using System;
+
+namespace SirRandoo.ToolkitUtils.Attributes
+{
+    /// <summary>
+    ///     Indicates that a given command requires a mod or a set of mods in
+    ///     order to function.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Class, AllowMultiple = true)]
+    public class RequiresModsAttribute : Attribute
+    {
+        public string[] ModIds;
+
+        public RequiresModsAttribute(params string[] modIds)
+        {
+            ModIds = modIds;
+        }
+    }
+}
---
+++ Source/ToolkitUtils.Api/Attributes/SubcommandAttribute.cs
@@ -0,0 +1,40 @@
+﻿// ToolkitUtils.Api
+// Copyright (C) 2022  SirRandoo
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+// 
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+using System;
+using SirRandoo.ToolkitUtils.Interfaces;
+
+namespace SirRandoo.ToolkitUtils.Attributes
+{
+    /// <summary>
+    ///     Decorates methods within an <see cref="ICommand"/> as a
+    ///     sub-command.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Method)]
+    public class SubcommandAttribute : Attribute
+    {
+        /// <summary>
+        ///     The text a viewer has to type after a command's text in order to
+        ///     execute the attached sub-command method.
+        /// </summary>
+        public string Name;
+
+        public SubcommandAttribute(string name)
+        {
+            Name = name;
+        }
+    }
+}
