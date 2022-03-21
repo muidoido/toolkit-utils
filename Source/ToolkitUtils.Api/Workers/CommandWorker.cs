9d3f1fbde3dc9a7a7a4cf1dec0de2a2391d68585
SirRandoo <8146689+SirRandoo@users.noreply.github.com>
2022-03-21 18:44:29 -0500

    Added an overload for backwards compatibility with Twitch Toolkit.

--- Source/ToolkitUtils.Api/Workers/CommandWorker.cs
+++ Source/ToolkitUtils.Api/Workers/CommandWorker.cs
@@ -15,6 +15,7 @@
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 using System;
+using System.Text;
 using System.Threading.Tasks;
 using CommonLib.Entities;
 using CommonLib.Interfaces;
@@ -22,8 +23,6 @@ using HarmonyLib;
 using JetBrains.Annotations;
 using SirRandoo.ToolkitUtils.Flags;
 using SirRandoo.ToolkitUtils.Interfaces;
-using SirRandoo.ToolkitUtils.Models;
-using SirRandoo.ToolkitUtils.Registries;
 using TwitchLib.Client.Models.Interfaces;
 using TwitchToolkit;
 
@@ -47,31 +46,55 @@ namespace SirRandoo.ToolkitUtils.Workers
         ///     Determines whether or not the given user can execute the given
         ///     command.
         /// </summary>
-        /// <param name="userId">The id of the user executing the command</param>
+        /// <param name="context">The context of the command's execution</param>
         /// <returns>Whether or not the user can use this command</returns>
         [NotNull]
-        public Task<bool> CanExecute([NotNull] string userId)
+        public Task<bool> CanExecute([NotNull] IContext context)
         {
-            if (!UserRegistry.TryGet(userId, out IUserData data))
+            if (!(context.User is IViewer viewer) || (_command.requiresAdmin && (viewer.UserType & UserTypes.Broadcaster) == 0))
             {
                 return Task.FromResult(false);
             }
 
-            if (_command.requiresAdmin && (data.UserType & UserTypes.Broadcaster) == 0)
+            return Task.FromResult(!_command.requiresMod || ((viewer.UserType & UserTypes.Broadcaster) != 0 && (viewer.UserType & UserTypes.Moderator) != 0));
+        }
+
+        [NotNull]
+        public async Task Execute(IContext context)
+        {
+            if (ToolkitSettings.CustomCommandDefs.Contains(_command.defName))
             {
-                return Task.FromResult(false);
+                Logger.Warn(
+                    new StringBuilder().Append($@"The command ""{_command.command}"" cannot be executed from this context; ")
+                       .Append("it uses Twitch Toolkit's custom command system, and as a result, requires a parameter unique to TwitchLib.")
+                       .ToString()
+                );
+
+                return;
             }
 
-            return Task.FromResult(!_command.requiresMod || ((data.UserType & UserTypes.Broadcaster) != 0 && (data.UserType & UserTypes.Moderator) != 0));
+            if (!(Activator.CreateInstance(_command.commandDriver) is ICommand instance))
+            {
+                Logger.Warn(
+                    $@"The command ""{_command.command}"" cannot be instantiated; it inherits from {_command.commandDriver.BaseType.FullDescription()} instead of {typeof(CommandDriver).FullDescription()}"
+                );
+
+                return;
+            }
+
+            await instance.ExecuteAsync(context);
         }
 
         /// <summary>
         ///     Executes the command asynchronously if a valid implementation
-        ///     exists,
-        ///     or synchronously if no other implementation exists.
+        ///     exists, or synchronously if no other implementation exists.
         /// </summary>
-        /// <param name="message"></param>
-        public async Task Execute(ITwitchMessage message)
+        /// <param name="message">
+        ///     The original Twitch message, as received from
+        ///     TwitchLib
+        /// </param>
+        /// <param name="context">The context of the given command's execution</param>
+        public async Task Execute(ITwitchMessage message, IContext context)
         {
             if (!(Activator.CreateInstance(_command.commandDriver) is CommandDriver instance))
             {
@@ -84,9 +107,7 @@ namespace SirRandoo.ToolkitUtils.Workers
 
             if (instance is ICommand command)
             {
-                var ctx = new CommandContext { User = UserRegistry.Get(message) };
-
-                await command.RunCommandAsync(ctx);
+                await command.ExecuteAsync(context);
             }
             else
             {
