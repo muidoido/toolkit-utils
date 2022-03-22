f683d530c5b0c765780ef851a236fe2a6d69488e
SirRandoo <8146689+SirRandoo@users.noreply.github.com>
2022-03-21 19:00:23 -0500

    Further outlined the IPlatform class.

--- Source/ToolkitUtils.Api/Interfaces/IPlatform.cs
+++ Source/ToolkitUtils.Api/Interfaces/IPlatform.cs
@@ -14,10 +14,17 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+using System.Threading.Tasks;
+using SirRandoo.ToolkitUtils.Flags;
+
 namespace SirRandoo.ToolkitUtils.Interfaces
 {
     public interface IPlatform
     {
         string PlatformId { get; }
+        Capabilities SupportedCapabilities { get; }
+
+        Task SendMessageAsync(string message);
+        Task SendPrivateMessageAsync(string user, string message);
     }
 }
